82bf46aa9f5cc22cd944de73e6ce9c3bc6ec72e4
Samridhi <samridhi@Samridhis-MacBook-Pro.local>
2022-04-04 01:38:09 +0200

    add queries

--- server/server.js
+++ server/server.js
@@ -7,12 +7,51 @@ const app = express();
 app.use(cors());
 
 app.get('/inputs', (req, res) => {
-  const TEST_QUERY = `SELECT Date FROM healthInputs.inputs WHERE Pain != 'none'`;
-  connection.query(TEST_QUERY, (error, response) => {
+  let queries = [
+    `SELECT Date FROM healthInputs.inputs WHERE Pain != 'none'`,
+    `SELECT Date FROM healthInputs.inputs WHERE Fatigue != 'none'`,
+    `SELECT Date FROM healthInputs.inputs WHERE Mood = 'highPos' OR Mood = 'medPos'`,
+    `SELECT Date FROM healthInputs.inputs WHERE Mood = 'highNeg' OR Mood = 'medNeg'`,
+    `SELECT Date FROM healthInputs.inputs WHERE Pain != 'none' AND Fatigue != 'none'`,
+    `SELECT Date FROM healthInputs.inputs WHERE Mood = 'highPos' OR Mood = 'medPos' AND Fatigue != 'none'`,
+    `SELECT Date FROM healthInputs.inputs WHERE Mood = 'highPos' OR Mood = 'medPos' AND Pain != 'none'`,
+  ];
+
+  connection.query(queries.join(';'), (error, response) => {
     if (error) {
       console.log(error);
     } else {
-      res.send({ pain: response });
+      res.send({ 
+        pain: response[0],
+        fatigue: response[1],
+        positive: response[2],
+        negative: response[3],
+        fatiguePain: response[4],
+        posFatigue: response[5],
+        posPain: response[6],
+      });
+    }
+  });
+});
+
+app.get('/aggregatedInputs', (req, res) => {
+  let queries = [
+    `SELECT Date FROM healthInputs.inputs WHERE Mood = 'highNeg' OR Mood = 'medNeg' AND Fatigue != 'none'`
+    `SELECT Date FROM healthInputs.inputs WHERE Mood = 'highNeg' OR Mood = 'medNeg' AND Pain != 'none'`,
+    `SELECT Date FROM healthInputs.inputs WHERE Mood = 'highPos' OR Mood = 'medPos' AND Fatigue != 'none' AND Pain != 'none'`,
+    `SELECT Date FROM healthInputs.inputs WHERE Mood = 'highNeg' and Mood = 'medNeg' AND Fatigue != 'none' AND Pain != 'none'`
+  ];
+
+  connection.query(queries.join(';'), (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send({ 
+        negFatigue: response[0],
+        negPain: response[1],
+        posFatiguePain: response[2],
+        negFatiguePain: response[3]
+      });
     }
   });
 });
